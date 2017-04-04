33874d888531353d0d5cbe511b7c26c546fc09a7
drew <drew.sullivan@decisiondesk.com>
2017-04-04 12:38:48 -0400

    cleaned up directories. removed ratchet, which was breaking things

---
+++ i_heart_meetings_mobile/iPhone/platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "pluginId": "org.apache.cordova.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "pluginId": "org.apache.cordova.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "pluginId": "org.apache.cordova.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.statusbar": "0.1.3"
+}
+// BOTTOM OF METADATA
+});
