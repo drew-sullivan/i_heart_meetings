abeb2bf26cd32ba465c62aa6c70a224630fa2b03
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-04-14 08:35:52 -0400

    added flask. added chart.js. got a little version working. going to plug my stuff in later today.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+from datetime import time
+ 
+  
+app = Flask(__name__)
+   
+    
+@app.route("/simple_chart")
+def chart():
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
+                     
+                     
+if __name__ == "__main__":
+    app.run(debug=False)
