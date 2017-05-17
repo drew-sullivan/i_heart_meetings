70e24b1d9841f36025d4d9ae0f99df22635e6c5e
drew <drew.sullivan@decisiondesk.com>
2017-05-17 12:52:57 -0400

    added script for removing uids from calendar files

--- i_heart_meetings/remove_uid.py
+++ i_heart_meetings/remove_uid.py
@@ -1,8 +1,25 @@
-#!/isr/bin/env python
+#!/usr/bin/python
+
+import sys
+import os
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
 
 bad_words = ['uid', 'UID']
 
-with open('oldfile.txt') as oldfile, open('newfile.txt', 'w') as newfile:
-    for line in oldfile:
-        if not any(bad_word in line for bad_word in bad_words):
-            newfile.write(line)
+filenames = os.listdir('ics_files')
+
+print(filenames)
+
+for filename in filenames:
+    with open(filename) as oldfile, open('new_' + filename , 'w') as newfile:
+        for line in oldfile:
+            if not any(bad_word in line for bad_word in bad_words):
+                newfile.write(line)
+
+    #  for arg in sys.argv[1:]:
+    #      with open(arg) as oldfile, open('new_' + arg , 'w') as newfile:
+    #          for line in oldfile:
+    #              if not any(bad_word in line for bad_word in bad_words):
+    #                  newfile.write(line)
