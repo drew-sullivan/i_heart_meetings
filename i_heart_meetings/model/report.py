7d734d796cd36ca72bcf72802ab32e1829edaf0c
drew <drew.sullivan@decisiondesk.com>
2017-07-06 15:30:46 -0400

    Removed broken survey from Slack post

--- i_heart_meetings/model/report.py
+++ i_heart_meetings/model/report.py
@@ -279,43 +279,54 @@ class Report:
 
     def post_report_to_slack(self):
         data = str(
-            {'text': self.summary_printout,
-                'attachments': [
-                    {
-                        "fallback": "Was this a good use of time and money?",
-                        "title": "Was this a good use of time and money?",
-                        "callback_id": "meetings_survey",
-                        "color": "#800080",
-                        "attachment_type": "default",
-                        "actions": [
-                            {
-                                "name": "yes",
-                                "text": "Yes",
-                                "type": "button",
-                                "value": "yes"
-                            },
-                            {
-                                "name": "no",
-                                "text": "No",
-                                "type": "button",
-                                "value": "no"
-                            },
-                            {
-                                "name": "maybe",
-                                "text": "I'm Not Sure",
-                                "type": "button",
-                                "value": "maybe"
-                            }
-                        ]
-                    }
-                ]
-            }
+            {'text': self.summary_printout}
         )
         url = self.SLACK_HOOK
         req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
         f = urllib2.urlopen(req)
         f.close()
 
+    #  TODO:
+    #      Add survey implementation
+    #  def post_report_to_slack(self):
+    #      data = str(
+    #          {'text': self.summary_printout,
+    #              'attachments': [
+    #                  {
+    #                      "fallback": "Was this a good use of time and money?",
+    #                      "title": "Was this a good use of time and money?",
+    #                      "callback_id": "meetings_survey",
+    #                      "color": "#800080",
+    #                      "attachment_type": "default",
+    #                      "actions": [
+    #                          {
+    #                              "name": "yes",
+    #                              "text": "Yes",
+    #                              "type": "button",
+    #                              "value": "yes"
+    #                          },
+    #                          {
+    #                              "name": "no",
+    #                              "text": "No",
+    #                              "type": "button",
+    #                              "value": "no"
+    #                          },
+    #                          {
+    #                              "name": "maybe",
+    #                              "text": "I'm Not Sure",
+    #                              "type": "button",
+    #                              "value": "maybe"
+    #                          }
+    #                      ]
+    #                  }
+    #              ]
+    #          }
+    #      )
+    #      url = self.SLACK_HOOK
+    #      req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+    #      f = urllib2.urlopen(req)
+    #      f.close()
+    #
 
     def set_summary(self):
         self.summary_printout = self.print_template.format(*self.printable_data)
